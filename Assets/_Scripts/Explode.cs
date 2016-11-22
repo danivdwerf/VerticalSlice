644c50cf35290c27657d4f48117214a2db22a512
Danivdwerf <danivdwerf.com>
2016-11-22 19:16:38 +0100

    added uv's to level mesh

---
+++ Assets/_Scripts/Explode.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explode : MonoBehaviour 
+{
+	private DestroySprite doIt;
+	private void Start()
+	{
+		doIt = GetComponent<DestroySprite> ();
+	}
+	private void Update()
+	{
+		if (Input.GetMouseButtonUp (0))
+		{
+			Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			point.z = 0; 
+			doIt.cutHole (Input.mousePosition.x,Input.mousePosition.y);
+		}
+	}
+}
