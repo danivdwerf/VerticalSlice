7a858b6580aa812ca3ec6a68f345f69bb1812c7e
Danivdwerf <danivdwerf.com>
2016-11-23 21:29:17 +0100

    destroyable floor

---
+++ Assets/_Scripts/BulletController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Classe responsável pelo código que controla a nossa bala
+public class BulletController : MonoBehaviour 
+{
+	public CircleCollider2D destructionCircle;
+	public static GroundController groundController;
+
+	void OnCollisionEnter2D( Collision2D coll )
+	{
+		Debug.Log (coll.collider.tag);
+		if( coll.collider.tag == "Ground" )
+		{
+			Debug.Log ("Destory");
+			groundController.DestroyGround( destructionCircle );
+			//Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/_Scripts/GroundController.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+public class GroundController : MonoBehaviour 
+{
+	private SpriteRenderer sr;
+	private Color hole; 
+	private float widthWorld, heightWorld;
+	private int widthPixel, heightPixel;
+
+	void Start()
+	{
+		//reference to sprite renderer
+		sr = GetComponent<SpriteRenderer>(); 
+		//Load in the texture
+		string path = "Assets/Sprites/LevelSprite.jpeg";
+		Texture2D tex = (Texture2D)AssetDatabase.LoadAssetAtPath(path,typeof(Texture2D));
+		//Create clone of the texture, so we won't alter(fuck up) the original
+		Texture2D tex_clone = (Texture2D) Instantiate(tex);
+		//Create a sprite from the cloned texture and set it as texture in the spriterenderer 
+		sr.sprite = Sprite.Create(tex_clone, new Rect(0f, 0f, tex_clone.width, tex_clone.height), new Vector2(0.5f, 0.5f), 100f);
+		//make a transparant colour(Color.clear would also work, but now you kind of make it yourself)
+		hole = new Color(0f, 0f, 0f, 0f);
+		//Set the properties according to the sizes of the new sprite 
+		SetDimensions();
+		BulletController.groundController = this;
+	}
+
+	private void SetDimensions() 
+	{
+		widthWorld = sr.bounds.size.x;
+		heightWorld = sr.bounds.size.y;
+		widthPixel = sr.sprite.texture.width;
+		heightPixel = sr.sprite.texture.height;
+	}
+
+	public void DestroyGround(CircleCollider2D cc)
+	{
+		V2int c = World2Pixel(cc.bounds.center.x, cc.bounds.center.y);
+		int r = Mathf.RoundToInt(cc.bounds.size.x*widthPixel/widthWorld);
+		int px, nx, py, ny;
+		
+		for (int i = 0; i <= r; i++)
+		{
+			int temp = (int)Mathf.RoundToInt(Mathf.Sqrt(r * r - i * i));
+			
+			for (int j = 0; j <= temp; j++)
+			{
+				px = c.x + i;
+				nx = c.x - i;
+				py = c.y + j;
+				ny = c.y - j;
+
+				sr.sprite.texture.SetPixel(px, py, hole);
+				sr.sprite.texture.SetPixel(nx, py, hole);
+				sr.sprite.texture.SetPixel(px, ny, hole);
+				sr.sprite.texture.SetPixel(nx, ny, hole);
+			}
+		}
+		sr.sprite.texture.Apply();
+		Destroy(GetComponent<PolygonCollider2D>());
+		gameObject.AddComponent<PolygonCollider2D>();
+	}
+
+	private V2int World2Pixel(float x, float y) 
+	{
+		V2int v = new V2int();
+		float dx = x-transform.position.x;
+		v.x = Mathf.RoundToInt(0.5f*widthPixel+ dx*widthPixel/widthWorld);
+		float dy = y - transform.position.y;
+		v.y = Mathf.RoundToInt(0.5f * heightPixel + dy * heightPixel / heightWorld);
+		
+		return v;
+	}
+}
