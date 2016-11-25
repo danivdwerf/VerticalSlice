5441e7ace7bae03ae0395e1cf461ec1550399f3d
brianboersen <19965@ma-web.nl>
2016-11-25 13:44:46 +0100

    begin van menu

---
+++ Assets/_Scripts/inventory/Dequip.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dequip : MonoBehaviour {
+    //last added weapon
+    GameObject previosAdd;
+
+	public void dequipLastWapon(GameObject gameobj)
+    {
+        //if there is a prevois gameobject
+        if(previosAdd != null)
+        {
+            //destroy the prevois gameobject
+            Destroy(previosAdd);
+        }
+        previosAdd = gameobj;
+    }
+}
---
+++ Assets/_Scripts/player_select/CurrentPlayer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CurrentPlayer : MonoBehaviour
+{
+    
+    [SerializeField]
+    private GameObject[] players;
+    [SerializeField]
+    private int Current = 0;
+
+   
+	
+	
+	public GameObject currentSelectedPlayer()
+    {        
+        return players[Current];
+    }
+
+    public void nextPlayer()
+    {
+        if(Current < players.Length - 1)
+        {
+            Current++;
+        }
+        else
+        {
+            Current = 0;
+        }
+    }
+}
