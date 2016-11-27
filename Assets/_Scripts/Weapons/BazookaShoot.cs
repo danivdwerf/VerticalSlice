527fa9f691af7f6d6ec369ecd42c3f931e2c0ff4
Danivdwerf <danivdwerf.com>
2016-11-27 13:54:06 +0100

    get input for shooting bazooka

---
+++ Assets/_Scripts/Weapons/BazookaShoot.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BazookaShoot : MonoBehaviour 
+{
+	//private GameObject bullet;
+	private float maxTime;
+	private float curTime;
+	private bool shooting;
+	private void Start()
+	{
+		shooting = false;
+		maxTime = 2f;
+	}
+
+	private void Update()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			shooting = true;
+		}
+		if(shooting)
+		{
+			updateShooting ();
+		}
+	}
+
+	private void updateShooting()
+	{
+		curTime += Time.deltaTime;
+		if (Input.GetKeyUp (KeyCode.Space))
+		{
+			shoot ();
+		}
+		if (curTime >= maxTime)
+		{
+			shoot ();
+		}
+	}
+
+	private void shoot ()
+	{
+		Debug.Log ("Shoot");
+		shooting = false;
+		curTime = 0;
+		return;
+	}
+}
