039518da4909043d0d038340be4c0d4a67ac23fc
Danivdwerf <danivdwerf.com>
2016-11-27 11:32:17 +0100

    Starting inventoryUI

--- Assets/_Scripts/GameController/Tags.cs
+++ Assets/_Scripts/GameController/Tags.cs
@@ -4,4 +4,5 @@ using System.Collections;
 public class Tags : MonoBehaviour 
 {
 	public const string ground = "Ground";
+	public const string gameController = "GameController";
 }
--- Assets/_Scripts/inventory/InventoryUI.cs
+++ Assets/_Scripts/inventory/InventoryUI.cs
@@ -5,14 +5,31 @@ public class InventoryUI : MonoBehaviour
 {
 	private Inventory inventory;
 	[SerializeField]private Button[] buttons;
+	[SerializeField]private GameObject inventoryPanel; 
 
 	private void Start()
 	{
 		inventory = GetComponent<Inventory> ();
+		inventoryPanel.SetActive (false);
 		for (int i = 0; i < buttons.Length; i++)
 		{
 			int temp = i;
 			buttons[i].onClick.AddListener (delegate(){inventory.SelectWeapon(temp);});
 		}
+	}
+
+	public void showInventory()
+	{
+		if (inventoryPanel.activeInHierarchy)
+		{
+			hideInventory ();
+			return;
+		}
+		inventoryPanel.SetActive (true);
+	}
+
+	public void hideInventory()
+	{
+		inventoryPanel.SetActive (false);
 	}
 }
