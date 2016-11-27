95dac45475770257a6c55d37ce21e89b8e7b0153
Danivdwerf <danivdwerf.com>
2016-11-27 13:28:12 +0100

    create aiming script

--- Assets/_Scripts/Weapons/BazookaAiming.cs
+++ Assets/_Scripts/Weapons/BazookaAiming.cs
@@ -3,16 +3,44 @@ using System.Collections;
 
 public class BazookaAiming : MonoBehaviour 
 {
+	private Transform thisTransform;
+	private float angle;
+	private float maxAngle;
+	private void Start()
+	{
+		angle = 0f;
+		maxAngle = 50f;
+	}
 	private void Update()
 	{
 		if(Input.GetKey(KeyCode.UpArrow))
 		{
-			updateAim ();
+			updateAim (1);
+			checkAngle ();
+		}
+		if (Input.GetKey (KeyCode.DownArrow))
+		{
+			updateAim (-1);
+			checkAngle ();
 		}
 	}
 
-	private void updateAim()
+	private void updateAim(int angleNum)
 	{
-		
+		thisTransform = this.transform;
+		this.transform.localEulerAngles = new Vector3 (0,0,angle);
+		angle+=angleNum;
+	}
+
+	private void checkAngle()
+	{
+		if (angle > maxAngle)
+		{
+			angle = maxAngle;
+		}
+		if (angle < -maxAngle)
+		{
+			angle = -maxAngle;
+		}
 	}
 }
