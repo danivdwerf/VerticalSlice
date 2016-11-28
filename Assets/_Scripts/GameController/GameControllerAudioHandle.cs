35e6da22fd54bc6e6b93e492aacf475dc97b1376
Danivdwerf <danivdwerf.com>
2016-11-28 18:07:55 +0100

    added sounds to the project

---
+++ Assets/_Scripts/GameController/GameControllerAudioHandle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControllerAudioHandle : MonoBehaviour 
+{
+	private AudioSource source;
+	private void Start()
+	{
+		source = GetComponent<AudioSource> ();
+	}
+	public void PlayAudio(AudioClip clip,bool looping)
+	{
+		source.clip = clip;
+		source.loop = looping;
+		source.Play ();
+	}
+}
--- Assets/_Scripts/Weapons/Bazooka/ProjectileCollision.cs
+++ Assets/_Scripts/Weapons/Bazooka/ProjectileCollision.cs
@@ -1,10 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEditor;
 
 public class ProjectileCollision : MonoBehaviour 
 {
 	[SerializeField]private GameObject circle;
-	private static GroundController groundController; 
+	private AudioClip explosionSound;
+	private GameControllerAudioHandle audioHandler;
+
+	private void Start()
+	{
+		string audioPath = "Assets/Audio/Effects/Explosion3.wav";
+		explosionSound = (AudioClip)AssetDatabase.LoadAssetAtPath (audioPath,typeof(AudioClip));
+		if (!explosionSound)
+		{
+			Debug.LogError ("Explosion audioclip is null");
+		}
+		audioHandler = GameObject.FindGameObjectWithTag (Tags.gameController).GetComponent<GameControllerAudioHandle> ();
+	}
 	private void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.gameObject.CompareTag (Tags.ground))
@@ -16,6 +29,7 @@ public class ProjectileCollision : MonoBehaviour
 	private void groundHit()
 	{
 		GameObject destroyGround = Instantiate (circle,this.transform.position,Quaternion.identity)as GameObject;
+		audioHandler.PlayAudio (explosionSound,false);
 		Destroy(gameObject);
 	}
 }
