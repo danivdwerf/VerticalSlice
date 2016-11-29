6b1ac12080e0b98c6ad0318a709d485608f7e772
JoramUiterwijk <joram1998@live.nl>
2016-11-29 10:25:56 +0100

    PlayerHealth WIP

---
+++ Assets/_Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField]
+    private float _maxHealth = 100;
+    private float _currentHealth; 
+    public float SetHealth
+    {
+        set
+        {
+            _currentHealth -= value;
+            Debug.Log(_currentHealth);
+        }
+    }
+    private bool _isDead;
+
+	void Start ()
+    {
+        _currentHealth = _maxHealth;
+        _isDead = false;
+	}
+	
+	void Update ()
+    {
+        if (_currentHealth < 1)
+        {
+            _isDead = true;
+        }
+        if(_isDead == true)
+        {
+            Destroy(gameObject.GetComponent("Player Movement"));
+        }
+        Debug.Log(_currentHealth);
+	}
+}
--- Assets/_Scripts/Weapons/Bazooka/BulletController.cs
+++ Assets/_Scripts/Weapons/Bazooka/BulletController.cs
@@ -5,16 +5,25 @@ public class BulletController : MonoBehaviour
 {
 	public static GroundController groundController;
 	private CircleCollider2D circle;
+
+    private PlayerHealth _playerHealth;
+
 	private void Start()
 	{
+        _playerHealth = GameObject.FindGameObjectWithTag(Tags.player).GetComponent<PlayerHealth>();
+
 		circle = GetComponent<CircleCollider2D> ();
 	}
 	void OnCollisionEnter2D( Collision2D coll )
 	{
-		if( coll.collider.tag == "Ground" )
+		if( coll.gameObject.CompareTag(Tags.ground))
 		{
 			groundController.DestroyGround(circle);
 			Destroy(gameObject);
 		}
+        if(coll.gameObject.CompareTag(Tags.player))
+        {
+            _playerHealth.SetHealth = 10;
+        }
 	}
 }
