653ee0bbafabbddf398d059930a1d53884535702
JoramUiterwijk <joram1998@live.nl>
2016-12-09 13:22:13 +0100

    EndTurn WIP

---
+++ Assets/_Scripts/Player/EndTurn.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndTurn : MonoBehaviour
+{
+    private int[] currentTurn = { 1, 2 };
+    private int arrayPos = 0;
+
+    private Timer timer;
+
+	void Start ()
+    {
+        timer = GetComponent<Timer>();
+	}
+	
+	void Update ()
+    {
+        if (timer.timeLeft <= 1f)
+        {
+            Debug.Log(currentTurn[arrayPos]);
+            if(arrayPos >= currentTurn.Length -1)
+            {
+                arrayPos = 0;
+            }
+            else
+            {
+                arrayPos += 1;
+            }
+        }
+
+	}
+}
