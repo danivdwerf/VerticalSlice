3727341398a13bd6ac35843816770adf990b498b
JoramUiterwijk <joram1998@live.nl>
2016-12-13 09:59:21 +0100

    cameraAndCanvas

---
+++ Assets/_Scripts/player_select/FollowPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private float distance;
+
+    void Update()
+    {
+        this.transform.position = target.position + new Vector3 (0,0,distance);
+    }
+}
