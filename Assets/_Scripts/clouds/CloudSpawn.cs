b59ba5bf2f22b705f6a68b980c300c0efcbb44be
brianboersen <19965@ma-web.nl>
2016-12-15 13:22:52 +0100

    clouds

---
+++ Assets/_Scripts/clouds/CloudSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] clouds;
+
+    private GameObject[] spawnedClouds;
+
+    private void Start()
+    {
+        if(clouds.Length > 0)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+
+               Instantiate(clouds[Random.Range(0,clouds.Length -1)], new Vector3(((Random.value + 0.1f) * 14) -7, ((Random.value + 0.1f) * 4) -1,1), Quaternion.identity);
+            }            
+        }
+        else
+        {
+            Debug.Log("add a cloud to the editor");
+        }
+    }
+}
---
+++ Assets/_Scripts/clouds/cloudmove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cloudmove : MonoBehaviour
+{
+    private float speed;
+
+    private void Start()
+    {
+        speed = Random.Range(1, 3);
+        speed = Random.value + 0.1f;
+    }
+
+	void Update ()
+    {
+        gameObject.transform.position += new Vector3(speed * Time.deltaTime,0,0);
+
+        if(gameObject.transform.position.x > 8)
+        {
+            gameObject.transform.position = new Vector3(-7, ((Random.value + 0.1f) * 4) - 1, 1);
+        }
+	}
+}
