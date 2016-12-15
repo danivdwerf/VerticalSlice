742cc40343c04f657f83d1d7a088d4a85c148fd8
brianboersen <19965@ma-web.nl>
2016-12-15 15:31:17 +0100

    fuse sound

--- Assets/_Scripts/GameController/Paths.cs
+++ Assets/_Scripts/GameController/Paths.cs
@@ -10,4 +10,5 @@ public class Paths : MonoBehaviour
 	public const string bazookadestructionPath = "Assets/prefabs/BazookaExplosionRange.prefab";
 	public const string explosion2Path = "Assets/Audio/Effects/Explosion2.wav";
 	public const string explosionParticlePath = "Assets/prefabs/Explosion.prefab";
+    public const string fusePath = "Assets/Audio/Effects/Fuse.wav";
 }
