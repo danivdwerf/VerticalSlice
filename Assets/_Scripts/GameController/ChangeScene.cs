b14d3ba83ff4e8ca2543013b480881044a0d4975
Danivdwerf <danivdwerf.com>
2016-12-19 12:37:05 +0100

    back to main menu

---
+++ Assets/_Scripts/GameController/ChangeScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour 
+{
+	public void change(int sceneIndex)
+	{
+		SceneManager.LoadScene (sceneIndex);
+	}
+}
--- Assets/_Scripts/Player/PlayerInput.cs
+++ Assets/_Scripts/Player/PlayerInput.cs
@@ -4,11 +4,13 @@ using System.Collections;
 public class PlayerInput : MonoBehaviour 
 {
 	private PlayerMovement movement;
+	private ChangeScene changeScene;
 	private float speed;
 	private void Start()
 	{
 		speed = 0.3f;
 		movement = GetComponent<PlayerMovement>(); 
+		changeScene = GameObject.FindGameObjectWithTag (Tags.gameController).GetComponentInParent<ChangeScene> ();
 	}
 	private void Update()
 	{
@@ -31,6 +33,10 @@ public class PlayerInput : MonoBehaviour
 		if (Input.GetKeyDown (KeyCode.Return))
 		{
             movement.Jump (3,2.5f);
+		}
+		if (Input.GetKeyDown (KeyCode.Escape))
+		{
+			changeScene.change (0);
 		}
 	}
 }
